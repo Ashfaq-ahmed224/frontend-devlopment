a6b85908730ac4db87af71c7e5f4aa99b8b171e9
Ashfaq-ahmed224 <168891934+Ashfaq-ahmed224@users.noreply.github.com>
2024-05-06 19:32:30 +0530

    fizz_buzz.js

--- assignment5/fizz_buzz.js
+++ assignment5/fizz_buzz.js
@@ -1,31 +1,15 @@
-//1) implement a fizzbuzz program that prints numbers from 1 to 100.
-//for (let i = 1; i <= 100; i++) {
-// console.log(i)
-//}
-
-
-//2) for multiples of 3, print "fizz" instead of the number.
-//3) for multiples of 5, print "buzz" instead of the number.
-//for (let i = 1; i <= 100; i++) {
-//  if (i % 3 == 0) {
-//     console.log(i, 'fizz')
-//}
-//else if (i % 5 == 0) {
-//   console.log(i, 'buzz');
-//}
-//}
 
 
 
-// 3) for numbers that are mutliples of both 3 and 5, print "fizzbuzz.js".
-// for (let i = 0; i <= 100; i++) {
+// 1) for numbers that are mutliples of both 3 and 5, print "fizzbuzz.js".
 
+for (let i = 0; i <= 100; i++) {
 
-//     if ((i % 3 == 0) && (i % 5 == 0)) {
-//         console.log(i, 'fizzbuzz')
-//     } else if (i % 3 == 0) {
-//         console.log(i, 'fizz')
-//     } else if (i % 5 == 0) {
-//         console.log(i, 'buzz')
-//     }
-// }
+     if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log(i, 'fizzbuzz')
+    } else if (i % 3 == 0) {
+         console.log(i, 'fizz')
+    } else if (i % 5 == 0) {
+         console.log(i, 'buzz')
+     }
+ }
